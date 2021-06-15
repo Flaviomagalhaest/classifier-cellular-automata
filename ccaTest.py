f2a91563cd56cd69563261ba34c54c01a6e14a05
Flavio Magalhães <flaviomagalhaest@gmail.com>
2021-06-15 18:46:14 -0300

    adding unittests and cca lib

---
+++ ccaTest.py
@@ -0,0 +1,57 @@
+import unittest
+import cca
+
+class returnNeighboringClassifiersTest(unittest.TestCase):
+   def setUp(self):
+      self.matrixTest = [
+         ["AA", "AB", "AC", "AD", "AE", "AF"],
+         ["BA", "BB", "BC", "BD", "BE", "BF"],
+         ["CA", "CB", "CC", "CD", "CE", "CF"],
+         ["DA", "DB", "DC", "DD", "DE", "DF"],
+         ["EA", "EB", "EC", "ED", "EE", "EF"],
+         ["FA", "FB", "FC", "FD", "FE", "FF"]
+      ]
+
+   def test_retorna_AB_BA_BB_DISTANCE1(self):
+      neighbors = cca.returnNeighboringClassifiers(6, 6, 0, 0, 1, self.matrixTest)
+      self.assertListEqual(["AB","BA","BB"], neighbors)
+
+   def test_retorna_AA_AB_AC_BA_BC_CA_CB_CC_DISTANCE1(self):
+      neighbors = cca.returnNeighboringClassifiers(6, 6, 1, 1, 1, self.matrixTest)
+      self.assertListEqual(["AA","AB","AC","BA","BC","CA","CB","CC"], neighbors)
+
+   def test_retorna_BC_BD_BE_CC_CE_DC_DD_DE_DISTANCE1(self):
+      neighbors = cca.returnNeighboringClassifiers(6, 6, 2, 3, 1, self.matrixTest)
+      self.assertListEqual(["BC","BD","BE","CC","CE","DC","DD","DE"], neighbors)
+
+   def test_retorna_DD_DE_DF_ED_EF_FD_FE_FF_DISTANCE1(self):
+      neighbors = cca.returnNeighboringClassifiers(6, 6, 4, 4, 1, self.matrixTest)
+      self.assertListEqual(["DD","DE","DF","ED","EF","FD","FE","FF"], neighbors)
+
+   def test_retorna_AB_AC_BA_BB_BC_CA_CB_CC_DISTANCE2(self):
+      neighbors = cca.returnNeighboringClassifiers(6, 6, 0, 0, 2, self.matrixTest)
+      self.assertListEqual(["AB","AC","BA","BB","BC","CA","CB","CC"], neighbors)
+
+   def test_retorna_AA_AB_AC_AD_BA_BC_BD_CA_CB_CC_CD_DA_DB_DC_DD_DISTANCE2(self):
+      neighbors = cca.returnNeighboringClassifiers(6, 6, 1, 1, 2, self.matrixTest)
+      self.assertListEqual(["AA","AB","AC","AD","BA","BC","BD","CA","CB","CC","CD","DA","DB","DC","DD"], neighbors)
+
+   def test_retorna_ABACADAEAF_BBBCBDBEBF_CBCCCECF_DBDCDDDEDF_EBECEDEEEF_DISTANCE2(self):
+      neighbors = cca.returnNeighboringClassifiers(6, 6, 2, 3, 2, self.matrixTest)
+      self.assertListEqual(["AB","AC","AD","AE","AF","BB","BC","BD","BE","BF","CB","CC","CE","CF","DB","DC","DD","DE","DF","EB","EC","ED","EE","EF"], neighbors)
+
+   def test_retorna_CCCDCECF_DCDDDEDF_ECEDEF_FCFDFEFF_DISTANCE2(self):
+      neighbors = cca.returnNeighboringClassifiers(6, 6, 4, 4, 2, self.matrixTest)
+      self.assertListEqual(["CC","CD","CE","CF","DC","DD","DE","DF","EC","ED","EF","FC","FD","FE","FF"], neighbors)
+
+   def test_retorna_AAABAC_BABC_CACBCC_DISTANCE1(self):
+      neighbors = cca.returnNeighboringClassifiers(4, 4, 1, 1, 1, self.matrixTest)
+      self.assertListEqual(["AA","AB","AC","BA","BC","CA","CB","CC"], neighbors)
+
+   def test_retorna_AAABACAD_BABCBD_CACBCCCD_DADBDCDDDISTANCE1(self):
+      neighbors = cca.returnNeighboringClassifiers(4, 4, 1, 1, 2, self.matrixTest)
+      self.assertListEqual(["AA","AB","AC","AD","BA","BC","BD","CA","CB","CC","CD","DA","DB","DC","DD"], neighbors)
+
+
+if __name__ == "__main__":
+   unittest.main()
---
+++ main.py
@@ -0,0 +1,80 @@
+import pandas as pd
+import random, cca
+
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+from sklearn.linear_model import SGDClassifier
+
+X, Y = make_classification(n_samples=100, n_classes=2, n_features=5, n_redundant=0, random_state=1)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+
+names = ["Nearest_Neighbors_3", "Nearest_Neighbors_4", "Nearest_Neighbors_5", "Nearest_Neighbors_7", "Linear_SVM", "Polynomial_SVM", "RBF_SVM", "Gaussian_Process",
+         "Gradient_Boosting", "Decision_Tree_3", "Decision_Tree_5", "Extra_Trees_10_2", "Extra_Trees_30_2", "Extra_Trees_10_4", "Random_Forest_12_100", "Random_Forest_15_100", "Random_Forest_5_300",
+         "Random_Forest_7_300", "Neural_Net", "AdaBoost_50", "AdaBoost_100", "AdaBoost_150",
+         "Naive_Bayes", "QDA", "SGD"]
+
+classifiers = [
+    KNeighborsClassifier(3),
+    KNeighborsClassifier(4),
+    KNeighborsClassifier(5),
+    KNeighborsClassifier(7),
+    SVC(kernel="linear", C=0.025),
+    SVC(kernel="poly", degree=3, C=0.025),
+    SVC(kernel="rbf", C=1, gamma=2),
+    GaussianProcessClassifier(1.0 * RBF(1.0)),
+    GradientBoostingClassifier(n_estimators=100, learning_rate=1.0),
+    DecisionTreeClassifier(max_depth=3),
+    DecisionTreeClassifier(max_depth=5),
+    ExtraTreesClassifier(n_estimators=10, min_samples_split=2),
+    ExtraTreesClassifier(n_estimators=30, min_samples_split=2),
+    ExtraTreesClassifier(n_estimators=10, min_samples_split=4),
+    RandomForestClassifier(max_depth=12, n_estimators=100),
+    RandomForestClassifier(max_depth=15, n_estimators=100),
+    RandomForestClassifier(max_depth=5, n_estimators=300),
+    RandomForestClassifier(max_depth=7, n_estimators=300),
+    MLPClassifier(alpha=1, max_iter=1000),
+    AdaBoostClassifier(n_estimators=50),
+    AdaBoostClassifier(n_estimators=100),
+    AdaBoostClassifier(n_estimators=150),
+    GaussianNB(),
+    QuadraticDiscriminantAnalysis(),
+    SGDClassifier(loss="hinge", penalty="l2")]
+
+classif = {}
+
+for name, clf in zip(names, classifiers):
+    print("Treinando "+name)
+    clf.fit(X_train, Y_train)
+    print("Clasificador "+name+" treinado.")
+    c = {}
+    c['name'] = name
+    c['score'] = clf.score(X_test, Y_test)
+    c['predict'] = clf.predict(X_test)
+    classif[name] = c
+
+#shuffling classifiers for the pool
+keys_classif = list(classif.keys())
+random.shuffle(keys_classif)
+
+#building matrix of first celullar automata
+matrix = [
+    cca.returnMatrixline(classif, keys_classif),
+    cca.returnMatrixline(classif, keys_classif),
+    cca.returnMatrixline(classif, keys_classif),
+    cca.returnMatrixline(classif, keys_classif),
+    cca.returnMatrixline(classif, keys_classif),
+]
+
+
+a = "a"
