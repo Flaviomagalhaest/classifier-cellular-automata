c5b3922e75f990da80374da8be4e6afe4cf89503
Flavio Magalhaes <flaviomagalhaest@gmail.com>
2022-08-26 10:35:43 -0300

    feat: configs

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 import yaml
 
 from app.pool_classifiers import get_all_classifiers
+from src.cca import learning_algorithm
 from src.dataset import Dataset
 from src.elements import Matrix, Pool
 from src.helpers import dict_results_to_dataframe
@@ -9,11 +10,12 @@ with open("src/cca_config.yaml", "r") as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
 
 all_classifiers = get_all_classifiers()
-dataset = Dataset("jm1", 1000, 1000)
-x_train, y_train = dataset.get_train_samples()
+dataset = Dataset("jm1", 1000, 1000, 1000)
+x_train_clf, y_train_clf = dataset.get_train_clf_samples()
+x_train_cca, y_train_cca = dataset.get_train_cca_samples()
 x_test, y_test = dataset.get_test_samples()
 pool = Pool(all_classifiers)
-pool.fit_all(x_train, y_train)
+pool.fit_all(x_train_clf, y_train_clf)
 pool.test_all(x_test, y_test)
 pool.remove_classifier_one_class()
 results = pool.get_results()
@@ -25,5 +27,17 @@ matrix = Matrix(
     size=config["matrix_size"],
     pool=pool,
     init_enery=config["init_energy"],
+    distance_neighborhood=config["distance"],
 )
+
+learning_algorithm(
+    matrix=matrix,
+    pool=pool,
+    sample_features=x_train_cca,
+    sample_class=y_train_cca,
+    distance=config["distance"],
+    interactions=config["interactions"],
+    init_energy=config["init_energy"],
+)
+matrix_class = matrix.predict_matrix(x_test)
 ...
